129a891204ac1962d445a81b2bbdccc5186c8fc4
dnitesh00x <nitesh@devoxtech.com>
2024-01-19 13:56:49 +0530

    dbhanu update code on git

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'functions/functions.dart';
+import 'functions/notifications.dart';
+import 'pages/loadingPage/loadingpage.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/services.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+  await Firebase.initializeApp();
+  checkInternetConnection();
+  initMessaging();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+
+  @override
+  Widget build(BuildContext context) {
+    platform = Theme.of(context).platform;
+    return GestureDetector(
+        onTap: () {
+          //remove keyboard on touching anywhere on the screen.
+          FocusScopeNode currentFocus = FocusScope.of(context);
+
+          if (!currentFocus.hasPrimaryFocus) {
+            currentFocus.unfocus();
+            FocusManager.instance.primaryFocus?.unfocus();
+          }
+        },
+        child: ValueListenableBuilder(
+            valueListenable: valueNotifierBook.value,
+            builder: (context, value, child) {
+              return MaterialApp(
+                  debugShowCheckedModeBanner: false,
+                  title: 'Tegxi User',
+                  theme: ThemeData(),
+                  home: const LoadingPage());
+            }));
+  }
+}
